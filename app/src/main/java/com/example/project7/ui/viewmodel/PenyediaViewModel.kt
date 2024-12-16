784d3db2831990eb17a9234d7aab956e3dbe5150
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-16 19:06:19 +0700

    untuk mengelola dependensi dan menyediakan akses ke berbagai bagian aplikasi, seperti repository, data, dan view models

--- app/src/main/java/com/example/project7/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project7/ui/viewmodel/PenyediaViewModel.kt
@@ -1,23 +1,38 @@
 package com.example.project7.ui.viewmodel
 
-import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.example.project7.KrsApp
 
-
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
     }
 }
 
-fun CreationExtras.krsApp(): KrsApp =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+fun CreationExtras.krsApp(): KrsApp = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
