9de58a9d15819dac87ca0629420ea9097cf8a579
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-16 18:02:47 +0700

    untuk mengelola data mahasiswa secara lokal di aplikasi, dengan menyediakan cara yang terstruktur untuk melakukan operasi CRUD di database

--- app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,32 @@ package com.example.project7.repository
 
 import com.example.project7.data.dao.MahasiswaDao
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    // Mendapatkan semua data mahasiswa dalam bentuk Flow
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    // Mendapatkan data mahasiswa berdasarkan NIM dalam bentuk Flow
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    // Menghapus data mahasiswa
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    // Memperbarui data mahasiswa
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
