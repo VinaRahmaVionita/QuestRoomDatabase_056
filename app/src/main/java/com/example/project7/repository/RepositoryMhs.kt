1dcf216dc9762e63ba84aab9466c8c6a948ba7f3
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-16 18:04:53 +0700

    untuk mendefinisikan operasi-operasi yang berhubungan dengan pengelolaan data mahasiswa

--- app/src/main/java/com/example/project7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/project7/repository/RepositoryMhs.kt
@@ -1,7 +1,28 @@
 package com.example.project7.repository
 
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
-    suspend fun insertMhs(mahasiswa: Mahasiswa)
+    //untuk mendapatkan semua data mahasiswa dalm bentuk aliran data (flow)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    //menganggil data mhs berdasarkan nim
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    //delete
+    suspend fun deleteMhs(
+        mahasiswa: Mahasiswa
+    )
+
+    //update
+    suspend fun updateMhs(
+        mahasiswa: Mahasiswa
+    )
+
+    //insert data
+    suspend fun insertMhs(
+        mahasiswa: Mahasiswa
+    )
+
 }
