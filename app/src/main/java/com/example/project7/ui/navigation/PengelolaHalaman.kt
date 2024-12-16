ac730d0da10660b58bee1491753b685ebce55d09
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-16 18:18:46 +0700

    menentukan halaman pertama ynag akan di tampilkan dalam aplikasi (start destinasi)

--- app/src/main/java/com/example/project7/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project7/ui/navigation/PengelolaHalaman.kt
@@ -4,27 +4,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.project7.ui.view.mahasiswa.DestinasiInsert
+import com.example.project7.ui.view.mahasiswa.DetailMhsView
+import com.example.project7.ui.view.mahasiswa.HomeMhsView
 import com.example.project7.ui.view.mahasiswa.InsertMhsView
+import com.example.project7.ui.view.mahasiswa.UpdateMhsView
 
 @Composable
 fun PengelolaHalaman(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ){
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiInsert.route
-    ) {composable(
-        route = DestinasiInsert.route
-    ){
-        InsertMhsView(
-            onBack = {}, onNavigate = { }
-        )
-    }
+        NavHost(
+            navController = navController,
+            startDestination = DestinasiHome.route) {
+
+            composable(
+            route = DestinasiHome.route
+            ){
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
+        }
+
 
     }
 }
