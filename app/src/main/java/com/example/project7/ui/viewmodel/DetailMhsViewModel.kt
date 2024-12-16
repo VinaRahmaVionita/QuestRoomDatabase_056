8574b48024371200c04d0b05537756fb415163dd
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-16 18:54:37 +0700

    mengelola tampilan detail mahasiswa, termasuk mengambil data mahasiswa berdasarkan NIM, menangani status loading dan error dan menghapus data mahasiswa jika diperlukan

--- app/src/main/java/com/example/project7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/project7/ui/viewmodel/DetailMhsViewModel.kt
@@ -18,7 +18,48 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit (DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit (
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
 
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
